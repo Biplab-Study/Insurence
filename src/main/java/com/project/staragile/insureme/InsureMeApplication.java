0c9284f362c518f5f64b61aa3a1614a616026ff9
Biplab-Study <130243249+Biplab-Study@users.noreply.github.com>
2023-07-06 20:07:48 +0530

    Update InsureMeApplication.java

--- src/main/java/com/project/staragile/insureme/InsureMeApplication.java
+++ src/main/java/com/project/staragile/insureme/InsureMeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InsureMeApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(InsureMeApplication.class, args);
 	}
 
